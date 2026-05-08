f81ddf69e6df944f3347f0487949631cc983ed81
Kristian Klette <geferg.dev@gmail.com>
2026-05-08 02:35:32 +0200

    chore: add placeholder header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
 import "./index.css";
 import NotFoundPage from "./pages/NotFoundPage";
 
 export function App() {
-    switch (window.location.pathname) {
+    return (
+        <AppLayout>
+            {renderPage(window.location.pathname)}
+        </AppLayout>
+    )
+}
+
+function renderPage(pathname: string) {
+    switch (pathname) {
         default: return <NotFoundPage />;
     }
+}
+
+function AppLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
