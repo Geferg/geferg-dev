a7f902d9af0fad9a4cae37d34bb2796d5250cc39
Kristian Klette <geferg.dev@gmail.com>
2026-08-06 22:17:58 +0200

    fix: rework project cards to squash hover jitter

--- src/pages/projects/projectGraphics/index.tsx
+++ src/pages/projects/projectGraphics/index.tsx
@@ -1,15 +1,20 @@
+import type { ProjectCardGraphicType } from "../projectData";
+
 import AudioGraphic from "./audioGraphic";
 import PlaceholderGraphic from "./placeholderGraphic";
 import RegexGraphic from "./regexGraphic";
 
-import type { ProjectCardGraphicType } from "../projectData";
-
 type ProjectCardGraphicProps = {
     type: ProjectCardGraphicType;
     accent: string;
+    active: boolean;
 };
 
-export default function ProjectGraphic({ type, accent }: ProjectCardGraphicProps) {
+export default function ProjectGraphic({
+    type,
+    accent,
+    active,
+}: ProjectCardGraphicProps) {
     switch (type) {
         case "regex": return <RegexGraphic accent={accent} />;
         case "audio": return <AudioGraphic accent={accent} />;
